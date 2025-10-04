e144c72786b86cdb4eff162bc69bf7071ed10876
mahdawemghr <mahdemghr2003@gmail.com>
2025-10-04 20:51:32 +0300

    deleting

--- lib/main.dart
+++ lib/main.dart
@@ -10,40 +10,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'UTip App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: null,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class UTip extends StatefulWidget {
+  const UTip({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<UTip> createState() => _UTipState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _UTipState extends State<UTip> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text('', style: Theme.of(context).textTheme.headlineMedium),
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
